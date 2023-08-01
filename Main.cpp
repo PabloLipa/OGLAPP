ec3d28c86e069d014ecf02c4d564c54ed38d5e73
pablo <pablohenriquessl326@outlook.com>
2023-08-01 14:27:16 -0300

    Adicionar arquivos de projeto.

---
+++ Main.cpp
@@ -0,0 +1,85 @@
+#include<filesystem>
+namespace fs = std::filesystem;
+
+#include"model.h"
+
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "OpenglAPP", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, width, height);
+
+
+
+	Shader shaderProgram("default.vert", "default.frag");
+
+	// Take care of all the light related things
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	shaderProgram.Activate();
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
+
+
+
+
+	
+	glEnable(GL_DEPTH_TEST);
+
+
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+	//std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
+	//std::string modelPath = "/ObjTest/cube.gltf";
+
+	// Load in a model
+	//Model model((parentDir + modelPath).c_str());
+
+	// Original code from the tutorial
+	Model model("ObjTest/cube.gltf");
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		camera.Inputs(window);
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+		// Draw a model
+		model.Draw(shaderProgram, camera);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	shaderProgram.Delete();
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
---
+++ VAO.cpp
@@ -0,0 +1,34 @@
+#include"VAO.h"
+
+// Construtor
+VAO::VAO()
+{
+	glGenVertexArrays(1, &ID);
+}
+
+
+void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset)
+{
+	VBO.Bind();
+	glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset);
+	glEnableVertexAttribArray(layout);
+	VBO.Unbind();
+}
+
+// Binds the VAO
+void VAO::Bind()
+{
+	glBindVertexArray(ID);
+}
+
+// Unbinds the VAO
+void VAO::Unbind()
+{
+	glBindVertexArray(0);
+}
+
+// Deletes the VAO
+void VAO::Delete()
+{
+	glDeleteVertexArrays(1, &ID);
+}
